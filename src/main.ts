74427544be8a4894b059f08d2cb40d82e27a2fb7
rodan0818 <rodanramdam0818@gmail.com>
2022-06-16 00:19:04 +0545

    properly implemented validation to blogDTO by useing ValidationPipe to the rootmodule

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    //whitelist feature will make sure  all those unwanted or invalid properties are stripped out and removed
+    // forbidNonWhitelisted throws error if unwanted properties are included in the body
+    // transform does change type according to the dto set to avoid problems
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
