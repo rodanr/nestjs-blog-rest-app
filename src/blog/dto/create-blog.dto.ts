70ce720c5e6855f108f2e84f43104a8e5d993c55
rodan0818 <rodanramdam0818@gmail.com>
2022-06-16 00:11:58 +0545

    added validator to DTO

--- src/blog/dto/create-blog.dto.ts
+++ src/blog/dto/create-blog.dto.ts
@@ -1,5 +1,10 @@
+import { IsString } from 'class-validator';
+
 export class CreateBlogDto {
+  @IsString()
   readonly title: string;
+  @IsString()
   readonly description: string;
+  @IsString()
   readonly author: string;
 }
